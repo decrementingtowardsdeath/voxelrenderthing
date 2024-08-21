8f371a195266379aab24f3158a95e40acd16eb74
BuildTools <unconfigured@null.spigotmc.org>
2024-08-21 15:08:23 +0200

    gjorde så att window.rs inte behöver vara mutable.

--- src/window.rs
+++ src/window.rs
@@ -1,8 +1,10 @@
+use std::cell::UnsafeCell;
+
 use minifb;
 
 pub struct Window {
-    window: minifb::Window,
-    frame_buffer: Vec<u32>,
+    window: UnsafeCell<minifb::Window>,
+    frame_buffer: UnsafeCell<Vec<u32>>,
     width: usize,
     height: usize,
 }
@@ -23,21 +25,21 @@ impl Window {
         let (width, height) = window.get_size();
         
         Self {
-            window: window,
-            frame_buffer: vec![0; width * height],
+            window: UnsafeCell::new(window),
+            frame_buffer: UnsafeCell::new(vec![0; width * height]),
             width: width,
             height: height,
         }
     }
 
     pub fn present(&mut self) {
-        match self.window.update_with_buffer(&self.frame_buffer, self.width, self.height) {
+        match self.window.get_mut().update_with_buffer(self.frame_buffer.get_mut().as_slice(), self.width, self.height) {
             Err(e) => println!("Failed to update framebuffer, {e}"),
             _ => {}
         }
     }
 
-    pub fn get_fb(&mut self) -> &mut [u32] {
-        &mut self.frame_buffer
+    pub fn access_framebuffer(&mut self, accessing_function: impl FnOnce(&mut [u32])) {
+        accessing_function(self.frame_buffer.get_mut().as_mut_slice());
     }
 }
